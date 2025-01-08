e9b97d3dfd3ca7aa21a3d1d9f380d2d49acac32a
xpornhot <ccursorboy@gmail.com>
2025-01-08 00:33:15 -0300

    fix: remove duplicate router and keep analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { Analytics } from '@vercel/analytics/react';
 import App from './App';
@@ -9,10 +8,8 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
-        <App />
-        <Analytics />
-      </BrowserRouter>
+      <App />
+      <Analytics />
     </HelmetProvider>
   </React.StrictMode>
 );
