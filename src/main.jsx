ded99fe5ed622f4b0281c95f76f78db95d45a186
xpornhot <ccursorboy@gmail.com>
2025-01-08 00:30:57 -0300

    feat: add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+        <Analytics />
+      </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>
 );
